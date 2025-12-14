b99ea39238dd46dbfe119b79e0282b524770f6e3
priz80 <priz8090@gmail.com>
2025-12-14 16:20:28 +0300

    Update ToDoList.tsx

--- src/components/ToDoList/ToDoList.tsx
+++ src/components/ToDoList/ToDoList.tsx
@@ -3,44 +3,36 @@ import "./ToDoList.scss"
 import { ToDo } from "../../models/todo-item"
 
 export const ToDoList = (props: {
-  todos: ToDo[],
-  updateToDo: Function,
-  deleteToDo: Function
+  todos: ToDo[]
+  updateToDo: (item: ToDo) => void
+  deleteToDo: (item: ToDo) => void
 }) => {
-  const checkedList = () => {
-    return props.todos
-      .filter((item) => !item.isDone)
-      .map((item, idx) => {
-        return (
-          <ToDoListItem
-            toDoItem={item}
-            key={idx}
-            updateToDo={props.updateToDo}
-            deleteToDo={props.deleteToDo}
-          />
-        );
-      });
-  };
+  const uncheckedList = props.todos
+    .filter(item => !item.isDone)
+    .map(item => (
+      <ToDoListItem
+        toDoItem={item}
+        key={item.id} // ✅ уникальный ключ
+        updateToDo={props.updateToDo}
+        deleteToDo={props.deleteToDo}
+      />
+    ))
 
-  const uncheckedList = () => {
-    return props.todos
-      .filter((item) => item.isDone)
-      .map((item, idx) => {
-        return (
-          <ToDoListItem
-            toDoItem={item}
-            key={idx}
-            updateToDo={props.updateToDo}
-            deleteToDo={props.deleteToDo}
-          />
-        );
-      });
-  };
+  const checkedList = props.todos
+    .filter(item => item.isDone)
+    .map(item => (
+      <ToDoListItem
+        toDoItem={item}
+        key={item.id} // ✅ уникальный ключ
+        updateToDo={props.updateToDo}
+        deleteToDo={props.deleteToDo}
+      />
+    ))
 
   return (
     <div className="todo-container">
-      <ul className="todo-list failed">{checkedList()}</ul>
-      <ul className="todo-list completed">{uncheckedList()}</ul>
+      <ul className="todo-list failed">{uncheckedList}</ul>
+      <ul className="todo-list completed">{checkedList}</ul>
     </div>
-  );
-};
+  )
+}
